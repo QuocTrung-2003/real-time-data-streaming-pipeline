c47cc05e59d231d052bd9d60825ced189134f6f8
Quoc Trung <lenq.trung@gmail.com>
2026-04-19 22:17:34 +0700

    update

--- services/api_service/config.py
+++ services/api_service/config.py
@@ -1,7 +1,7 @@
 import os
 
-DB_HOST = os.getenv("DB_HOST", "postgres")
-DB_PORT = os.getenv("DB_PORT", "5432")
-DB_NAME = os.getenv("DB_NAME", "db")
-DB_USER = os.getenv("DB_USER", "user")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "pass")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
