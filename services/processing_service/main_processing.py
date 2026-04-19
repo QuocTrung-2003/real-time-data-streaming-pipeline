ed4a60639177af5edb1cbc71c74f6ec257a81d5b
Quoc Trung <lenq.trung@gmail.com>
2026-04-19 10:30:20 +0700

    feat: initialize spark streaming job with kafka source

---
+++ services/processing_service/main_processing.py
@@ -0,0 +1,53 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import (
+    col, avg, max, window, to_timestamp,
+    when
+)
+from pyspark.sql.functions import first
+from consumer import read_kafka_stream
+from transformer import parse_event
+from validator import validate
+from processing_service.config import CHECKPOINT_PATH
+
+
+
+
+
+# =========================
+# MAIN PIPELINE
+# =========================
+def main():
+
+    spark = SparkSession.builder \
+        .appName("WeatherProcessingService") \
+        .config(
+            "spark.jars.packages",
+            ",".join([
+                "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0",
+                "org.postgresql:postgresql:42.7.3"
+            ])
+        ) \
+        .getOrCreate()
+
+    spark.sparkContext.setLogLevel("WARN")
+
+    # 1. Kafka stream
+    df = read_kafka_stream(spark)
+
+   
+
+    # =========================
+    # 9. DEBUG CONSOLE
+    # =========================
+    console_query = aggregated_df.writeStream \
+        .outputMode("complete") \
+        .format("console") \
+        .option("checkpointLocation", CHECKPOINT_PATH + "/console") \
+        .trigger(processingTime="20 seconds") \
+        .start()
+
+    spark.streams.awaitAnyTermination()
+
+
+if __name__ == "__main__":
+    main()
